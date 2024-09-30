a218304eca10c00d1b505d28cadb62de82552e8b
Omar Hawwash <81506354+OmarMHawash@users.noreply.github.com>
2024-09-30 13:28:08 +0300

    Time: 44 ms (87.56%) | Memory: 16.7 MB (62.41%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.py
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -1,11 +1,11 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
         row = 0; direction = -1
-        rows = [[] for _ in range(numRows)]
+        rows = [''] * numRows
 
         i = 0
         while(len(s) > i):
-            rows[row].append(s[i])
+            rows[row]+= s[i]
             if row < numRows-1 and direction == -1:
                 row+=1
             if row > 0 and direction == 1:
@@ -14,4 +14,4 @@ class Solution:
                 direction *= -1
             i+=1
 
-        return ''.join(''.join(c) for c in rows)
+        return ''.join(rows)
