ce2d2ae30996b256c4c1577d2bf73a2ded3aaf4f
Omar Hawwash <81506354+OmarMHawash@users.noreply.github.com>
2024-12-05 09:14:53 +0200

    Time: 0 ms (100.00%) | Memory: 42 MB (35.04%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        // define size, interators, result
+        StringBuilder result = new StringBuilder();
+        int size1 = word1.length();
+        int size2 = word2.length();
+        int w1count = 0;
+        int w2count = 0;
+        
+        // add alternately:
+        while (w1count < size1 || w2count < size2){
+            if (w1count < size1){
+                result.append(word1.charAt(w1count));
+                w1count++;
+            }
+            if (w2count < size2){
+                result.append(word2.charAt(w2count));
+                w2count++;
+            } 
+        }
+
+        return result.toString();
+    }
+}
